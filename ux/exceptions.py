24e9236d20522a54e0f8b77ba4a38d2eb50302dd
Sam Aldis <sam.aldis@ukjp-design.com>
2017-05-26 11:41:13 +0800

    Started adding UI, making sure its interchangable

--- ux/exceptions.py
+++ ux/exceptions.py
@@ -1,8 +1,14 @@
-
+# PARSER ERRORS - Can be used to swap out for different Locales
 _EXCEPTION_FILE_NOT_SET = "No file set for the parser"
 _EXCEPTION_FILE_NOT_FOUND = "The file could not be found!"
 _EXCEPTION_FILE_EMPTY = "The loaded file was empty"
 
-
 class ParserError(Exception):
+    pass
+
+# UI Processing Errors
+_EXCEPTION_MAIN_WINDOW_NOT_FOUND = "The main window hasn't been created"
+_EXCEPTION_NON_UI_OBJECT = "The object passed was not a ui object"
+
+class UIError(Exception):
     pass
--- ux/parser.py
+++ ux/parser.py
@@ -16,6 +16,11 @@ class Parser:
                 raise e.ParserError(e._EXCEPTION_FILE_NOT_FOUND)
         except Exception as error:
             raise e.ParserError(error)
+    ''' 
+    The ShowUI function loads the file passed when the class is initiated
+    and parses the UI contained within it to process through an external module
+    (External so that it can be swapped out at a later date for something different if required)
+    '''
     def showUI(self):
         ui_data = self._begin_parse()
         
