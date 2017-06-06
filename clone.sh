480d0e21a45d4e591ad330150ebb6a7f60491bef
Sam Aldis <sam.aldis@ukjp-design.com>
2017-06-06 16:19:47 +0800

    added clone script

---
+++ clone.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/sam-aldis/pyUX
+git clone https://github.com/sam-aldis/pyUXStudio
+cd pyUX
+pip install -r requirements.pip
+npm install
+cd ../
+cd pyUXStudio
+npm install
